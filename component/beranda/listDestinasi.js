8d16642906d49c308bf16a28af2ecb0672cb485d
apriadi <apriadi@bts.id>
2021-10-04 11:28:49 +0700

    list destinasi

--- component/beranda/listDestinasi.js
+++ component/beranda/listDestinasi.js
@@ -1,38 +1,28 @@
 const ListDestinasi = () => {
+    const imageLists = [
+        "https://i.pinimg.com/originals/a6/4b/c3/a64bc3f5af8933d18f1e01dbfdc35bde.jpg",
+        "https://dolanyok.com/wp-content/uploads/2019/05/gunung-putri-lembang-bandung.jpg",
+        "https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1619012092/uale9cs278cj33e9if8t.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/f/f0/Tugu_pdg.jpg",
+        "https://www.indonesia.travel/content/dam/indtravelrevamp/home-revamp/Semarang-Thumbnail-v1.jpg",
+        "https://www.indonesia.travel/content/dam/indtravelrevamp/home-revamp/Solo-Thumbnail-v1.jpg",
+        "https://www.indonesia.travel/content/dam/indtravelrevamp/home-revamp/Yogyakarta-Thumbnail-v1.jpg",
+        "https://www.indonesia.travel/content/dam/indtravelrevamp/home-revamp/Lake-Toba-Thumbnail-v1.jpg",
+        "https://www.indonesia.travel/content/dam/indtravelrevamp/home-revamp/Labuan-Bajo-Thumbnail-v1.jpg",
+        "https://www.indonesia.travel/content/dam/indtravelrevamp/home-revamp/Lombok-Thumbnail-v1.jpg"
+    ];
+
     return(
         <div className="mt-10 ml-10">
             <p className="text-3xl font-serif font-bold text-gray-600">Destinasi Pilihan</p>
             <div className="grid grid-cols-2 md:grid-cols-5 my-5 gap-5 mr-10">
-                <div>
-                    <img className="h-72" src="https://i.pinimg.com/originals/a6/4b/c3/a64bc3f5af8933d18f1e01dbfdc35bde.jpg"/>
-                </div>
-                <div>
-                    <img className="h-72" src="https://dolanyok.com/wp-content/uploads/2019/05/gunung-putri-lembang-bandung.jpg"/>
-                </div>
-                <div>
-                    <img className="h-72" src="https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1619012092/uale9cs278cj33e9if8t.jpg"/>
-                </div>
-                <div>
-                    <img className="h-72" src="https://upload.wikimedia.org/wikipedia/commons/f/f0/Tugu_pdg.jpg"/>
-                </div>
-                <div>
-                    <img className="h-72" src="https://www.indonesia.travel/content/dam/indtravelrevamp/home-revamp/Semarang-Thumbnail-v1.jpg"/>
-                </div>                
-                <div>
-                    <img className="h-72" src="https://www.indonesia.travel/content/dam/indtravelrevamp/home-revamp/Solo-Thumbnail-v1.jpg"/>
-                </div> 
-                <div>
-                    <img className="h-72" src="https://www.indonesia.travel/content/dam/indtravelrevamp/home-revamp/Yogyakarta-Thumbnail-v1.jpg"/>
-                </div> 
-                <div>
-                    <img className="h-72" src="https://www.indonesia.travel/content/dam/indtravelrevamp/home-revamp/Lake-Toba-Thumbnail-v1.jpg"/>
-                </div>
-                <div>
-                    <img className="h-72" src="https://www.indonesia.travel/content/dam/indtravelrevamp/home-revamp/Labuan-Bajo-Thumbnail-v1.jpg"/>
-                </div>
-                <div>
-                    <img className="h-72" src="https://www.indonesia.travel/content/dam/indtravelrevamp/home-revamp/Lombok-Thumbnail-v1.jpg"/>
-                </div>
+                {
+                    imageLists.map((item, index) => 
+                        <div key={index}>
+                            <img className="h-72 rounded-sm" src={item}/>
+                        </div>
+                    )
+                }
                 
             </div>
         </div>
